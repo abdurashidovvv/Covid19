e3400ea9bbb73ca28d3b56521161b589d87c7d7f
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-05-17 18:43:23 +0500

    RecycleView

---
+++ app/src/main/java/uz/abdurashidov/covid19/ui/adapter/CountryAdapter.kt
@@ -0,0 +1,26 @@
+package uz.abdurashidov.covid19.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import uz.abdurashidov.covid19.databinding.CountryRvItemBinding
+import uz.abdurashidov.covid19.models.covidmodels.RawData
+
+class CountryAdapter(val list: ArrayList<RawData>):RecyclerView.Adapter<CountryAdapter.Vh>() {
+
+    inner class Vh(rvItem: CountryRvItemBinding):RecyclerView.ViewHolder(rvItem.root){
+        fun onBind(rawData: RawData){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vh {
+        return Vh(CountryRvItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: Vh, position: Int) {
+        holder.onBind(list[position])
+    }
+}
