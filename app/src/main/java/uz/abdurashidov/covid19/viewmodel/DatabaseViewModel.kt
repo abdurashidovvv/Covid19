87e38d096ad847c917018d479817c5d7a9cd1ffd
Saydullo <abdurashidovsaydullo35@gmail.com>
2023-05-23 14:28:25 +0500

    DatabaseViewModel is finished.!

--- app/src/main/java/uz/abdurashidov/covid19/viewmodel/DatabaseViewModel.kt
+++ app/src/main/java/uz/abdurashidov/covid19/viewmodel/DatabaseViewModel.kt
@@ -1,7 +1,42 @@
 package uz.abdurashidov.covid19.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import uz.abdurashidov.covid19.database.entity.Article
+import uz.abdurashidov.covid19.repository.DatabaseRepository
+import uz.abdurashidov.covid19.utils.Resource
+import uz.abdurashidov.covid19.utils.Status
+import javax.inject.Inject
 
 @HiltViewModel
-class DatabaseViewModel {
+class DatabaseViewModel @Inject constructor(
+    private val databaseRepository: DatabaseRepository,
+) : ViewModel() {
+
+    private val stateFlow =
+        MutableStateFlow<Resource<List<Article>>>(Resource(Status.LOADING, null, "Loading"))
+
+    fun getAllUsers(): MutableStateFlow<Resource<List<Article>>> {
+        viewModelScope.launch {
+            stateFlow.value = Resource(Status.SUCCESS, databaseRepository.getArticles(), "Success")
+        }
+        return stateFlow
+    }
+
+    fun addArticle(article: Article) {
+        viewModelScope.launch {
+            databaseRepository.addArticle(article)
+        }
+    }
+
+    fun deleteArticle(article: Article) {
+        viewModelScope.launch {
+            databaseRepository.deleteArticle(article)
+        }
+    }
+
+
 }
